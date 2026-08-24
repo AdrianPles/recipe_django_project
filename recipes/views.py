3dfaf8cf9f7cfbbaf8b3314e60c233aa29101d4d
AdrianPles <adi_kite@yahoo.com>
2026-08-24 17:18:53 +0300

    added search bar

--- recipes/views.py
+++ recipes/views.py
@@ -9,10 +9,14 @@ def home(request: HttpRequest):
     return HttpResponse("Welcome to my webserver!")
 
 def list_recipes(request: HttpRequest):
-    # am declarat variabilele 'by' si 'sort' pentru a realiza doua label-uri de filtrare/sortare
+    # am declarat variabilele 'by' si 'sort' pentru a realiza doua label-uri de filtrare/sortare, iar 'query' este variabila care se ocupa de cautarea cuvantului cheie din bara de cautare.
+    query = request.GET.get("q")
     by = request.GET.get("by", "title")
     sort = request.GET.get("sort", "asc")
     recipes = Recipe.objects.all()
+    # __icontains caută cuvântul oriunde în titlu și ignoră literele mari/mici
+    if query:
+        recipes = recipes.filter(title__icontains=query)
     # pentru o sortare corecta dupa 'titlu reteta' folosim functia Django/Lower
     if by == "title":
         if sort == "asc":
